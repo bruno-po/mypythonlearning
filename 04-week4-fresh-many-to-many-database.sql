f40ca02d9c6a4b12fea66df36618cf9c40cf764d
Bruno Pinheiro <bruno.lahunmen@gmail.com>
2017-02-06 17:05:41 -0200

    inclusão do código para inserir dados de courses e users

--- 04-week4-fresh-many-to-many-database.sql
+++ 04-week4-fresh-many-to-many-database.sql
@@ -17,3 +17,14 @@ CREATE TABLE Member (
 		role INTEGER,
 	PRIMARY KEY (user_id, course_id)
 )
+
+INSERT INTO User (name, email) VALUES ('Jane',
+'jane@tsugi.org');
+INSERT INTO User (name, email) VALUES ('Ed',
+'ed@tsugi.org');
+INSERT INTO User (name, email) VALUES ('Sue',
+'sue@tsugi.org');
+
+INSERT INTO Course (title) VALUES ('Python');
+INSERT INTO Course (title) VALUES ('SQL');
+INSERT INTO Course (title) VALUES ('PHP')
