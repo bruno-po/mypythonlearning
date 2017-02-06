663a831a63ba3995620979470ea86e190b9eecf4
Bruno Pinheiro <bruno.lahunmen@gmail.com>
2017-02-06 17:27:24 -0200

    busca com SELECT

--- 04-week4-fresh-many-to-many-database.sql
+++ 04-week4-fresh-many-to-many-database.sql
@@ -36,3 +36,9 @@ INSERT INTO Member (user_id, course_id, role) VALUES (1, 2, 0);
 INSERT INTO Member (user_id, course_id, role) VALUES (2, 2, 1);
 INSERT INTO Member (user_id, course_id, role) VALUES (2, 3, 1);
 INSERT INTO Member (user_id, course_id, role) VALUES (3, 3, 0)
+
+SELECT User.name Member.role Course.title
+	FROM User JOIN Member JOIN Course
+	ON Member.user_id = User.id
+		AND Member.course_id = Course.id
+	ORDER BY Course.title, Member.role DESC, User.name
