e981ccad698849f0062f6b1f23ce48f00c175888
Bruno Pinheiro <bruno.lahunmen@gmail.com>
2017-02-06 17:16:34 -0200

    inclusão dos members e roles

--- 04-week4-fresh-many-to-many-database.sql
+++ 04-week4-fresh-many-to-many-database.sql
@@ -28,3 +28,11 @@ INSERT INTO User (name, email) VALUES ('Sue',
 INSERT INTO Course (title) VALUES ('Python');
 INSERT INTO Course (title) VALUES ('SQL');
 INSERT INTO Course (title) VALUES ('PHP')
+
+INSERT INTO Member (user_id, course_id, role) VALUES (1, 1, 1);
+INSERT INTO Member (user_id, course_id, role) VALUES (2, 1, 0);
+INSERT INTO Member (user_id, course_id, role) VALUES (3, 1, 0);
+INSERT INTO Member (user_id, course_id, role) VALUES (1, 2, 0);
+INSERT INTO Member (user_id, course_id, role) VALUES (2, 2, 1);
+INSERT INTO Member (user_id, course_id, role) VALUES (2, 3, 1);
+INSERT INTO Member (user_id, course_id, role) VALUES (3, 3, 0)
