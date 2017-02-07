23c848079e6b9d5561186d4dc8e0ce6a13821bd6
Bruno Pinheiro <bruno.lahunmen@gmail.com>
2017-02-07 16:58:45 -0200

    arquivos gerados pelo código

---
+++ dbgmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','umich.edu','virginia.edu','cam.ac.uk','unicon.net','mtu.edu','berkeley.edu','unl.edu','etudes.org','columbia.edu','ucmerced.edu'],
+['2005-12',8,5,4,4,3,3,3,2,2,2]
+];
