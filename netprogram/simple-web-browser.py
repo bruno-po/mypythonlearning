4a085b81a1e87fcff96c72b19305f0b16da61124
Bruno Pinheiro <bruno.lahunmen@gmail.com>
2017-02-07 23:35:07 -0200

    netprogram: primeiro arquivo - cópia do código do navegador simples

---
+++ netprogram/simple-web-browser.py
@@ -0,0 +1,9 @@
+import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('www.getip.net.br', 80))
+mysock.send('GET http://getip.net.br/pesquisadores HTTP/1.0\n\n')
+while True:
+    data = mysock.recv(512)
+    if ( len(data) < 1 ) : break
+print data
+mysock.close()
