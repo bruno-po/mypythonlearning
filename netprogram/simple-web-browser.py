f4e9abbb36dc193b4daade00275a105ace7fb369
Bruno Pinheiro <bruno.lahunmen@gmail.com>
2017-02-08 00:07:00 -0200

    netprogram: navegado básico com link atualizado

--- netprogram/simple-web-browser.py
+++ netprogram/simple-web-browser.py
@@ -2,7 +2,7 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('www.getip.net.br', 80))
-mysock.send('GET http://getip.net.br/pesquisador HTTP/1.0\n\n')
+mysock.send('GET http://getip.net.br/pesquisa HTTP/1.0\n\n')
 
 while True:
     data = mysock.recv(512)
